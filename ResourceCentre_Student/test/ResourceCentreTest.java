6bb78c7b618c1be76adffbde5206791c03304db2
BlackNet13 <123053395+BlackNet13@users.noreply.github.com>
2023-07-12 14:04:11 +0800

    test

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -6,7 +6,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 // C206 AY2023 SEM1 - Eclipse 21-09 JDK16
-// test yo
 
 public class ResourceCentreTest {
 	// prepare test data
